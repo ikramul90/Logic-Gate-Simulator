23ece668d2ad2cdb4942215762cdbc65e2340272
ikramul <ikramulalamchowdhury2@gmail.com>
2026-08-29 12:00:25 +0600

    added all the gates with input validation in the python file

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+def input_validation(prompt, var1, var2):
+    while True:
+        validate_input = input(prompt + "Which operation do you want to use? \n"
+                "1. AND operation \n"
+                "2. OR operation \n"
+                "3. NOT operation \n"
+                "4. NAND operation \n"
+                "5. NOR operation \n")
+
+        if validate_input == "1":
+            return ANDgate(var1, var2)
+        elif validate_input == "2":
+            return ORgate(var1,var2)
+        elif validate_input == "3":
+            print("Note: NOT gate only uses the first input.")
+            return NOTgate(var1)
+        elif validate_input == "4":
+            return NANDgate(var1,var2)
+        elif validate_input == "5":
+            return NORgate(var1,var2)
+        else:
+            print("Invalid input. Please try again.")
+
+
+
 def parse_input(prompt):
     high_inputs = ["1", "High", "high", "Yes", "yes", "On", "on", "True", "true"]
     low_inputs = ["0", "Low", "low", "No", "no", "Off", "off", "False", "false"]
@@ -17,7 +42,7 @@ def ANDgate(a, b):
     return a & b
 
 def ORgate(a, b):
-    return a or b
+    return a | b
 
 def NOTgate(a):
     return int(not a)
@@ -34,3 +59,5 @@ var1 = parse_input("First input: ")
 var2 = parse_input("Second input: ")    
 
 
+validate = input_validation("--- LOGIC GATE MENU ---\n", var1, var2)
+print("The final output is: ", validate)
