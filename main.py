34f8a1265d20ab0471ee5c60343d7ee420ce6fa3
ikramul <ikramulalamchowdhury2@gmail.com>
2026-08-29 11:28:59 +0600

    added the gates in python

--- main.py
+++ main.py
@@ -11,9 +11,26 @@ def parse_input(prompt):
             return 0
         else:
             print("Invalid input. Please try again.")
-    
-number1 = parse_input("First input: ")
-number2 = parse_input("Second input: ")    
 
-total = number1 & number2
-print("The total is:", total)
+
+def ANDgate(a, b):
+    return a & b
+
+def ORgate(a, b):
+    return a or b
+
+def NOTgate(a):
+    return int(not a)
+
+def NANDgate(a, b):
+  return int(not ANDgate(a, b))
+
+def NORgate(a, b):
+    return int(not ORgate(a,b))
+
+
+
+var1 = parse_input("First input: ")
+var2 = parse_input("Second input: ")    
+
+
